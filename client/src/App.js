200846896b5a7c91ac021842ceb7cc2694a641b0
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-11-03 16:46:28 -0800

    App and Index and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './Images/memories.png'
+import useStyle from './style';
 
-const App =()=> {
+const App = () => {
+    const classes = useStyle();
     return (
-        <div>
-            <hi>
-                App
-            </hi>
-        </div>
+        <Container maxidth="lg">
+            <AppBar className={classes.AppBar} position="static" color="inherit">
+                <Typography classes={classes.heading}variant="h2" align="center">Memories</Typography>
+                <img classes={classes.image}src={memories} alt="memories" height="60"></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+
+            </Grow>
+
+        </Container>
     )
 }
 
