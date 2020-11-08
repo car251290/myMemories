a58933be56b81033e30a2b5e3028963090f2c70e
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-11-08 14:08:52 -0800

    Add files via upload

--- client/components/Form/Form.js
+++ client/components/Form/Form.js
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import {TextField,Button,Typography,Paper} from '@material-ui/core';
+import useStyle from './styles';
 const Form =()=> {
+    const classes = useStyle();
+
+    const handleSubmit = () =>{
+
+    }
     return(
-        <hi>FORM</hi>
+        <Paper className={classes.paper}>
+            <form autoComplete = 'off' noValidate className ={classes.form} onSubmit={handleSubmit}>
+                <Typography variant="h6">Creating a great Memory </Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidth/>
+                </form>
+        </Paper>
     );
 }
 
