60f5ccee080a82736339b3ebe37b8d82dfad2b5e
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-11-30 21:05:31 -0800

    Add files via upload

--- client/components/Form/Form.js
+++ client/components/Form/Form.js
@@ -1,29 +1,38 @@
-import React,{useState} from 'react';
-import {TextField,Button,Typography,Paper} from '@material-ui/core';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import useStyle from './styles';
-const Form =()=> {
-    const[postData,setPostData] = useState({
-        creator: '', title: '',message:'',tags:'',selectedFile:'',
+const Form = () => {
+    const [postData, setPostData] = useState({
+        creator: '',
+        title: '',
+        message: '',
+        tags: '',
+        selectedFile: ''
 
     });
     const classes = useStyle();
 
-    const handleSubmit = () =>{
+    const handleSubmit = () => {
+
 
     }
-    return(
-        <Paper className={classes.paper}>
-            <form autoComplete = 'off' noValidate className ={classes.form} onSubmit={handleSubmit}>
-                <Typography variant= "h6" >Creating a great Memory </Typography>
-                <TextField 
-                name= "creator" 
-                variant= "outlined" 
-                 label= "Creator" 
-                 fullWidth
-                value = (e) => setPostData({ creator: e.target.value })
-                />
-                </form>
+    return (
+         <Paper className = { classes.paper } >
+        <form autoComplete = 'off'
+        noValidate className = { classes.form }
+        onSubmit = { handleSubmit } >
+        <Typography variant = "h6" > Creating a great Memory </Typography>  
+        <TextField name = "creator"
+        variant = "outlined"
+        label = "Creator"
+        fullWidth value = { postData.creator }
+        onChange = {(e) => setPostData({ creator: e.target.value })
+        }
+        />   
+        </form > 
         </Paper>
+
+        
     );
 }
 
