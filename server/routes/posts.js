fddb933a044e8695a943ab875074eb88d56c6d38
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-12-13 14:59:16 -0800

    commit 3

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import {getPosts,createPost} from '../controllers/posts.js';
+import {getPosts,createPost,updatePost} from '../controllers/posts.js';
 const router = express.Router();
 
 
-
+// for the back end and for the concept CRUD
 router.get('/',getPosts);
 router.post('/',createPost);
-
+router.patch('/:id',updatePost)
 
 export default router;
