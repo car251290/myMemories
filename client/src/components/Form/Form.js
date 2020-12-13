d0e98fdc6aea0d31a3c2015bbccb3c571e7e014c
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-12-13 15:02:11 -0800

    commi 5

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,35 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
-import useStyle from './styles';
-import FileBase from "react-file-base64"
-import {useDispatch} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
+import FileBase from 'react-file-base64';
 
-import {createPost} from '../../actions/posts'
+import useStyles from './styles';
+import { createPost, updatePost } from '../../actions/posts';
 
-const Form = () => {
-    const [postData, setPostData] = useState({creator: '',title: '',message: '',tags: '',selectedFile: ''
-    
-    });
-    const classes = useStyle();
-     const dispatch = useDispatch();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(createPost(postData))
-    }
+const Form = ({ currentId, setCurrentId }) => {
+    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+    const dispatch = useDispatch();
+    const classes = useStyles();
+  
+    useEffect(() => {
+      if (post) setPostData(post);
+    }, [post]);
+  
     const clear = () => {
+      setCurrentId(0);
+      setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      if (currentId === 0) {
+        dispatch(createPost(postData));
+        clear();
+      } else {
+        dispatch(updatePost(currentId, postData));
+        clear();
+      }
+    };
 
-    }
     return (
          <Paper className = { classes.paper } >
         <form autoComplete = 'off'
         noValidate className = { `${classes.root} ${classes.form}`}
-        onSubmit = { handleSubmit } >
-            <Typography  variant = "h6" >
-            Creating a great Memory
-            </Typography>
+        onSubmit = { handleSubmit }>
+           <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
         <TextField name = "creator" variant = "outlined" label = "Creator"fullWidth value = { postData.creator }onChange = {(e) => setPostData({ ...postData,creator: e.target.value })}/>
           <TextField name = "title" variant = "outlined" label = "tile"fullWidth value = { postData.title }onChange = {(e) => setPostData({ ...postData,title: e.target.value })}/>
+         
          <TextField name = "message" variant = "outlined" label = "message"fullWidth value = { postData.message }onChange = {(e) => setPostData({ ...postData,messager: e.target.value })}/>
          <TextField name = "tags" variant = "outlined" label = "tags"fullWidth value = { postData.tags }onChange = {(e) => setPostData({ ...postData,tags: e.target.value })}/>   
          <div className={classes.fileInput}>
