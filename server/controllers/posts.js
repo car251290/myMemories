b7b1b0b9ffbad4c42b50fe4b17ccc8c6f37546d2
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-12-28 16:41:01 -0800

    Add files via upload

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req,res) => {
@@ -29,4 +30,38 @@ export const createPosts=(req,res)=>{
 
 export const createPost =(req,res)=> {
     res.send('Post Creation');
+}
+
+
+export const updatePost = async(req,res) => {
+    const {id:_id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(_id))return res.status(404).send(`no post with that return id`)
+   
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post,_id}, {new:true});
+    res.json(updatedPost);
+
+}
+
+
+export const deletePost = async(req,res) => {
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))return res.status(404).send(`no post with that return id`)
+
+    await PostMessage.findByIdAndRemove(id);
+    console.log('DELETE')
+
+    res.json({message: 'post deleted successfully'});
+
+
+}
+
+export const likePost = async(req,res) => {
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))return res.status(404).send(`no post with that return id`)
+    const post = await  PostMessage.findById(id);
+
+    const updatePost = await PostMessage.findByIdAndUpdate(id,{likeCount:post.likeCount + 1},{new : true})
+    
+    res.json(updatePost);
+
 }
