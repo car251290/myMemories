eb1d66c35e118681a415d2549752deb139e9c4f3
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-02-01 18:31:33 -0800

    commit 1

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 import express from 'express';
-<<<<<<< HEAD
+
 import {getPosts,createPost,updatePost,deletePost,likePost} from '../controllers/posts.js';
-=======
+
 import {getPosts,createPost,updatePost} from '../controllers/posts.js';
->>>>>>> 8c2482ea4ffa89bd240e81d92beb0f308174aea3
+
 const router = express.Router();
 
 
@@ -11,12 +11,12 @@ const router = express.Router();
 router.get('/',getPosts);
 router.post('/',createPost);
 router.patch('/:id',updatePost)
-<<<<<<< HEAD
+
 router.delete('/:id',deletePost)
 router.patch('./:id/likePost',likePost)
 
 export default router;
-=======
+
 
 export default router;
->>>>>>> 8c2482ea4ffa89bd240e81d92beb0f308174aea3
+
