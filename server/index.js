6e731ce48c763922ab51b60c718d0b710ef69df2
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-11-24 17:39:08 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,22 +3,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
 import postRoutes from './routes/posts.js';
-import dotenv from 'dotenv';
+
 const app = express();
-dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts',postRoutes);
-
-
+app.use('/posts', postRoutes);
+//https://data.mongodb-api.com/app/data-twguz/endpoint/data/beta
+const CONNECTION_URL = 'mongodb+srv://Javi:love123@cluster0.qhhir.mongodb.net/MemoryProject?';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
